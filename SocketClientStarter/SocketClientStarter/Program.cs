1ad614eb3b109cebe5c251e1c130cb943e6565c9
JSR103 <silva.joel.r@gmail.com>
2017-06-26 13:23:50 -0700

    Saving a Client and Server console app that communicate

---
+++ SocketClientStarter/SocketClientStarter/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClientStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = null;
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = null;
+
+            try
+            {
+                Console.WriteLine("Welcome to my Socket Client Starter Example");
+                Console.WriteLine("Please enter a valid IP Address and press enter");
+
+                string strIPAddress = Console.ReadLine();
+
+                Console.WriteLine("Please supply a valid Port Number 0 - 65535 and Press Enter");
+
+                string strPortInput = Console.ReadLine();
+
+                int nPortInput = 0;
+
+                if (!IPAddress.TryParse(strIPAddress, out ipaddr))
+                {
+                    Console.WriteLine("Invalid server IP Address supplied");
+                    return;
+                }
+
+                if (!int.TryParse(strPortInput.Trim(), out nPortInput))
+                {
+                    Console.WriteLine("Invalid Port supplied");
+                    return;
+                }
+
+                if (nPortInput <= 0 || nPortInput > 65535)//Makes sure that the port is in range 
+                {
+                    Console.WriteLine("Port Number must be between 0 and 65535");
+                    return;
+                }
+
+                System.Console.WriteLine(string.Format("IPAddress: {0} - Port{1} ", ipaddr.ToString(), nPortInput));
+
+                client.Connect(ipaddr, nPortInput);//literally connecting to the server
+
+                //Console.ReadKey();
+
+                Console.WriteLine("Connected to the sever, type text and press enter to send it to the server, type <EXIT> to close");
+
+                string inputCommand = string.Empty;
+
+                while (true)//as long as true this while loop will send what the client inputs and sends to the server
+                {
+                    inputCommand = Console.ReadLine();
+
+
+                    if (inputCommand.Equals("<EXIT>"))
+                    {
+                        break;//Exits the while Loop
+                    }
+
+                    byte[] buffsend = Encoding.ASCII.GetBytes(inputCommand);
+
+                    client.Send(buffsend);
+
+                    byte[] buffReceived = new byte[128];
+                    int nRec = client.Receive(buffReceived);
+
+                    Encoding.ASCII.GetString(buffReceived, 0, nRec);
+
+                    Console.WriteLine("Data received: {0}", Encoding.ASCII.GetString(buffReceived, 0, nRec));
+
+
+                }
+
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+            }
+            finally//Under here is what exits the app by pressing any key
+            {
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);
+
+                    }
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SocketServerStarter/SocketServerStarter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+namespace SocketServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPAddress ipaddr = IPAddress.Any;
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            try
+            {
+
+
+
+                listenerSocket.Bind(ipep);
+
+                listenerSocket.Listen(5);
+
+                Socket client = listenerSocket.Accept();
+
+                Console.WriteLine("Client Connected. " + client.ToString() + " IP End Point: " + client.RemoteEndPoint.ToString());
+
+                byte[] buff = new byte[128];//basically how big/size your "thing" that your trying to send can be..if that makes sense 
+
+                int numberOfReceivedBytes = 0;
+
+                while (true)
+                {
+
+                    numberOfReceivedBytes = client.Receive(buff);
+
+                    Console.WriteLine("Number of received bytes: " + numberOfReceivedBytes.ToString());
+
+                    Console.WriteLine("Data sent by client is: " + buff);
+
+                    string recivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
+
+                    Console.WriteLine("Data sent by client is: " + recivedText);
+
+                    client.Send(buff);//Once you press a key if will be sent right away
+
+                    if (recivedText == "x")
+                    {
+                        break;//Closes the app if buff is x
+                    }
+
+                    Array.Clear(buff, 0, buff.Length);//Cleans
+
+                    numberOfReceivedBytes = 0; //sets it back to 0 for no future problems 
+
+
+
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+            }
+
+        }
+    }
+}
